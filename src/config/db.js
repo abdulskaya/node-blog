ec962beefb6e5f4de5ac7c8997bd6b6a9516eef6
Abdulsamet KAYA <abdulskaya@gmail.com>
2022-03-19 16:54:30 +0300

    db parameter error fix

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const db_config = require('./config.json')
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize(db_config.database, db_config.username, db_config.password, {
+const connection = new Sequelize(db_config.development.database, db_config.development.username, db_config.development.password, {
   host: db_config.development.HOST,
   dialect: db_config.development.dialect,
   port : process.env.MYSQL_CONNECTION_STRING,
@@ -9,8 +9,8 @@ const connection = new Sequelize(db_config.database, db_config.username, db_conf
 });
 
 if (connection.authenticate()) {
-  console.log('Veritabanı bağlantısı başarılıs');
+  console.log('Db connected successfully');
 }else {
-  console.log('Veritabanı bağlantısı başarısız');
+  console.log('Db connection failed');
 }
 module.exports = connection;
